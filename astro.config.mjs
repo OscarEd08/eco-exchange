2fdafbab42fcda4650b7d1289280635dede8924d
Oscar Pairazaman <70730776+OscarEd08@users.noreply.github.com>
2025-08-15 15:05:27 -0500

    Update Astro config and remove Welcome component
    Set the 'site' and 'base' options in astro.config.mjs for deployment configuration. Removed the Welcome.astro component, likely as part of project cleanup or to remove default starter content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://OscarEd08.github.io',
+    base: 'eco-exchange', 
+});
